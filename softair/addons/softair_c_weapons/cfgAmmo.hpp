ae1cef10eb1251ae97dbd9ce15622775d1d455f2
Kllrt <kllrtik@gmail.com>
2017-12-31 17:01:39 +0100

    BB should now be visible for longer time, edited hiteffects to remove spark and blood effects

--- softair/addons/softair_c_weapons/cfgAmmo.hpp
+++ softair/addons/softair_c_weapons/cfgAmmo.hpp
@@ -1,6 +1,10 @@
 class CfgAmmo
 {
-	class B_556x45_Ball;
+	class BulletBase;
+	class B_556x45_Ball: BulletBase
+	{
+		class HitEffects;
+	};
 	class softair_B_6mm_BB : B_556x45_Ball
 	{
 		hit                = 1;
@@ -14,5 +18,15 @@ class CfgAmmo
 		model              = "\softair\addons\softair_weapons\ammo\6mm_bb";
 		tracerStartTime    = 0;
 		tracerScale        = 3;
+		tracerEndTime      = 20;
+		cartridge          = "";
+		class HitEffects: HitEffects
+		{
+			hitVirtual    = "ImpactPlastic";
+			hitMetalPlate = "ImpactPlastic";
+			hitMetal      = "ImpactPlastic";
+			hitMan        = "ImpactPlastic";
+			hitBuilding   = "ImpactPlastic";
+		};
 	};
 };
